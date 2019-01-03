12687dfd78a8c638befee372f9dee69f9ce9349b
Chetan <chetan.mouselangelo@gmail.com>
2019-01-03 15:44:42 +0100

    Add video

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from "React";
 import { View, StyleSheet } from "react-native";
+import Video from "react-native-video";
+
+const videoURL =
+  "https://cdn.flowkey.com/courses/3-muster-der-schwarzen-tasten-en_2016-03-17_12-03-16.mp4";
 
 export default class App extends Component {
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <Video style={styles.video} source={{ uri: videoURL }} />
+      </View>
+    );
   }
 }
 
@@ -12,6 +20,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#222222"
+    backgroundColor: "#000000"
+  },
+  video: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
   }
 });
