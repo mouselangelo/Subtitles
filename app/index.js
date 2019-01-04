838d96d215fa4bcb129b6d5d1ce87cf7d2fd7e28
Chetan <chetan.mouselangelo@gmail.com>
2019-01-04 16:38:49 +0100

    Add Audio track selection

--- app/index.js
+++ app/index.js
@@ -2,8 +2,16 @@ import React, { Component } from "React";
 import { View, Text, Button, Picker, StyleSheet, Platform } from "react-native";
 import Video, { TextTrackType } from "react-native-video";
 
-const videoURL =
-  "https://devstreaming-cdn.apple.com/videos/wwdc/2016/504m956dgg4hlw2uez9/504/hls_vod_mvp.m3u8"; //WWDC 2016: What's new in HLS
+const videos = [
+  "https://devstreaming-cdn.apple.com/videos/wwdc/2016/504m956dgg4hlw2uez9/504/hls_vod_mvp.m3u8", //WWDC 2016: What's new in HLS - multiple subtitles
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8", // multiple audio tracks & subtitles
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", // 4x3 aspect ratio
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8", // Advanced stream (HEVC/H.264) fMP4
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8", // Advanced stream TS
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8" //Advanced stream fMP4
+];
+
+const videoURL = videos[1];
 
 export default class App extends Component {
   state = {
@@ -11,8 +19,10 @@ export default class App extends Component {
     errorMessage: null,
     audioTracks: [],
     textTracks: [],
+    selectedSubtitle: "auto",
+    selectedAudioTrackIndex: 0,
     showSubtitlePicker: false,
-    selectedSubtitle: null
+    showAudioTrackPicker: false
   };
 
   onVideoLoading = () => {
@@ -20,14 +30,11 @@ export default class App extends Component {
   };
 
   onVideoLoaded = ({ audioTracks, textTracks }) => {
-    const subtitles = this.getSubtitleTracks(textTracks);
-    // could also select a subtitle based on language
-    selectedSubtitle = subtitles.length > 0 ? subtitles[0].title : null;
+    console.log(audioTracks, textTracks);
     this.setState({
-      textTracks: subtitles,
+      textTracks,
       audioTracks,
-      loading: false,
-      selectedSubtitle: selectedSubtitle
+      loading: false
     });
   };
 
@@ -50,16 +57,53 @@ export default class App extends Component {
     }
   };
 
-  renderToolBar() {
+  getSelectedTextTrack = title => {
+    if (!title) {
+      return {
+        type: "disabled"
+      };
+    }
+    if (title === "auto") {
+      return {
+        type: "system"
+      };
+    }
+    return {
+      type: "title",
+      value: title
+    };
+  };
+
+  renderToolBar(hasSubtitles, hasAudioTracks) {
+    if (!hasSubtitles && !hasAudioTracks) {
+      return null;
+    }
     return (
       <View style={styles.toolbar}>
-        <Button
-          title="Subtitles"
-          style={styles.button}
-          onPress={() => {
-            this.setState({ showSubtitlePicker: true });
-          }}
-        />
+        {hasAudioTracks && (
+          <Button
+            title="Audio Tracks"
+            style={styles.button}
+            onPress={() => {
+              this.setState({
+                showAudioTrackPicker: true,
+                showSubtitlePicker: false
+              });
+            }}
+          />
+        )}
+        {hasSubtitles && (
+          <Button
+            title="Subtitles"
+            style={styles.button}
+            onPress={() => {
+              this.setState({
+                showAudioTrackPicker: false,
+                showSubtitlePicker: true
+              });
+            }}
+          />
+        )}
       </View>
     );
   }
@@ -69,6 +113,10 @@ export default class App extends Component {
     if (!(textTracks && textTracks.length > 0)) {
       return null;
     }
+
+    const subtitles = textTracks.filter(item => {
+      return item.title;
+    });
 
     return (
       <View style={{ backgroundColor: "#e0e0e0", padding: 20 }}>
@@ -88,7 +136,40 @@ export default class App extends Component {
           }}
         >
           <Picker.Item label="None" key="none" value="none" />
-          {textTracks.map(({ title }) => {
+          <Picker.Item label="Auto (Recommended)" key="auto" value="auto" />
+          {subtitles.map(({ title }) => {
+            return <Picker.Item label={title} key={title} value={title} />;
+          })}
+        </Picker>
+      </View>
+    );
+  }
+
+  renderAudioTrackPicker() {
+    const { audioTracks, selectedAudioTrackIndex } = this.state;
+    if (!(audioTracks && audioTracks.length > 0)) {
+      return null;
+    }
+
+    const selectedValue = audioTracks[selectedAudioTrackIndex].title;
+
+    return (
+      <View style={{ backgroundColor: "#e0e0e0", padding: 20 }}>
+        <Text style={styles.dialogTitle}>Audio Tracks</Text>
+        <Picker
+          mode="dialog"
+          style={{ minWidth: 240, minHeight: 240 }}
+          prompt="Audio Tracks"
+          title="Audio Tracks"
+          selectedValue={selectedValue}
+          onValueChange={(_, itemIndex) => {
+            this.setState({
+              showAudioTrackPicker: false,
+              selectedAudioTrackIndex: itemIndex
+            });
+          }}
+        >
+          {audioTracks.map(({ title }) => {
             return <Picker.Item label={title} key={title} value={title} />;
           })}
         </Picker>
@@ -102,19 +183,20 @@ export default class App extends Component {
       errorMessage,
       textTracks,
       showSubtitlePicker,
-      selectedSubtitle
+      selectedSubtitle,
+      audioTracks,
+      selectedAudioTrackIndex,
+      showAudioTrackPicker
     } = this.state;
 
     const hasSubtitles = !loading && textTracks.length > 0;
+    const hasAudioTracks = !loading && audioTracks.length > 1;
+    const selectedTextTrack = this.getSelectedTextTrack(selectedSubtitle);
 
-    const selectedTextTrack = selectedSubtitle
-      ? {
-          type: "title",
-          value: selectedSubtitle
-        }
-      : {
-          type: "disabled"
-        };
+    const selectedAudioTrack = {
+      type: "index",
+      value: selectedAudioTrackIndex
+    };
 
     return (
       <View style={styles.container}>
@@ -128,13 +210,17 @@ export default class App extends Component {
           onError={this.onVideoError}
           onLoad={this.onVideoLoaded}
           selectedTextTrack={selectedTextTrack}
+          selectedAudioTrack={selectedAudioTrack}
         />
         {loading && <Text style={styles.loadingText}>Loading...</Text>}
         {errorMessage && (
           <Text style={styles.errorText}>Oops!! {errorMessage}</Text>
         )}
-        {hasSubtitles && this.renderToolBar()}
+        {this.renderToolBar(hasSubtitles, hasAudioTracks)}
         {hasSubtitles && showSubtitlePicker && this.renderSubtitlePicker()}
+        {hasAudioTracks &&
+          showAudioTrackPicker &&
+          this.renderAudioTrackPicker()}
       </View>
     );
   }
